87b1fbd8de99ffcf2684c28495d819dc7633c666
Tamal Patra <patra.tamal@gmail.com>
2016-04-03 14:57:12 +0530

    Initial Upload

--- MatrixCharlieplex.cpp
+++ MatrixCharlieplex.cpp
@@ -5,12 +5,13 @@
 
 /* 
  * File:   MatrixCharlieplex.cpp
- * Description: Drive (N * (N-1)) number of "Charlieplxed" diode matrices
+ * Description: Drive (N * (N-1)) number of "Charlieplexed" diode (node) matrices
  *              (e.g. LED Matrix) with N number of outputs capable of tri-state.
- *              It
+ *              The grid consists of X-axis (horizontal) as Anodes & Y-axis
+ *              (vertical) as Cathodes. Here the grid is displayed below.
  *
- *                   C A T H O D E
- *                   1 2 3 4 5 . N
+ *                (-)C A T H O D E
+ *             (+)   1 2 3 4 5 . N
  *              A  1 X 0 0 0 0
  *              N  2 0 X 0 0 0
  *              O  3 0 0 X 0 0
@@ -18,6 +19,22 @@
  *              E  5 0 0 0 0 X
  *                 .           X
  *                 N             X
+ * 
+ *              There will be N number of rows and (N-1) number of columns. The 
+ *              same line cannot be connected to both Anode & Cathode of the 
+ *              same node. Hence, the "X" is marked, where they cannot be both
+ *              at same time.
+ * 
+ *              It's best practice to connect the Anodes in same row & then 
+ *              connect the Cathodes.
+ * 
+ *              e.g. Cathode Connection for a 5 pin setup
+ *                   (-)C A T H O D E
+ *              R  1 -> 2 3 4 5
+ *              O  2 -> 1 3 4 5
+ *              W  3 -> 1 2 4 5
+ *              S  4 -> 1 2 3 5
+ *                 5 -> 1 2 3 4
  * 
  * Author: Tamal Patra <patra.tamal@gmail.com>
  * 
@@ -27,12 +44,12 @@
 #include "Arduino.h"
 #include "MatrixCharlieplex.h"
 
-namespace MatrixCharlieplex {
+namespace ArduinoMatrixCharlieplex {
 #pragma region Constructor
 
     MatrixCharlieplex::MatrixCharlieplex(uint8_t pins[]) {
         this->_pins = pins;
-        this->_noOfPins = sizeof (pins) / sizeof (uint8_t);
+        this->_noOfPins = sizeof(pins)/sizeof(uint8_t);
         this->_maxNode = this->_noOfPins * (this->_noOfPins - 1);
         this->_activeNode = new DiodeNode();
         this->_init(false);
@@ -41,10 +58,10 @@ namespace MatrixCharlieplex {
 
 #pragma region Private Functions
 
-    MatrixCharlieplex::_init(boolean isReset) {
+    void MatrixCharlieplex::_init(boolean isReset) {
         // Sink all output pins
         for (uint8_t i = 0; i < this->_noOfPins; i++) {
-            _sinkPin(*(this->_pins + i));
+            _sinkPin(*(this->_pins+i));
         }
         this->_activeNode->vcc = 0;
         this->_activeNode->gnd = 0;
@@ -53,78 +70,143 @@ namespace MatrixCharlieplex {
 
     // Help from http://www.instructables.com/id/Charlieplexing-the-Arduino/
 
-    MatrixCharlieplex::_upPin(uint8_t pin) {
+    boolean MatrixCharlieplex::_upPin(uint8_t pin) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, HIGH);
+//        Serial.print(pin);
+//        Serial.println("-going up");
         return true;
     }
 
-    MatrixCharlieplex::_downPin(uint8_t pin) {
+    boolean MatrixCharlieplex::_downPin(uint8_t pin) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
+//        Serial.print(pin);
+//        Serial.println("-going down");
         return true;
     }
 
-    MatrixCharlieplex::_sinkPin(uint8_t pin) {
+    boolean MatrixCharlieplex::_sinkPin(uint8_t pin) {
         pinMode(pin, INPUT);
         digitalWrite(pin, LOW);
+//        Serial.print(pin);
+//        Serial.println("-going sink");
         return true;
     }
 
-    MatrixCharlieplex::_setNode(DiodeNode* pin, uint8_t state) {
+    boolean MatrixCharlieplex::_setNode(DiodeNode* pin, uint8_t state) {
         if (state) {
-            switch (this->_activeNode->vcc) {
-                case pin->vcc:
-                    break;
-                case pin->gnd:
-                    _downPin(pin->gnd);
-                    break;
-                default:
-                    if (this->_state)
-                        _sinkPin(this->_activeNode->vcc);
-                    break;
-            };
-            switch (this->_activeNode->gnd) {
-                case pin->vcc:
-                    _upPin(pin->vcc);
-                    break;
-                case pin->gnd:
-                    break;
-                default:
-                    if (this->_state)
-                        _sinkPin(this->_activeNode->gnd);
-                    break;
-            };
+            uint8_t _chkMatch = 0;
+            //            switch (this->_activeNode->vcc) {
+            //                case pin->vcc:
+            //                    break;
+            //                case pin->gnd:
+            //                    _downPin(pin->gnd);
+            //                    break;
+            //                default:
+            //                    if (this->_state)
+            //                        _sinkPin(this->_activeNode->vcc);
+            //                    break;
+            //            };
+            if (this->_activeNode->vcc == pin->vcc) {
+                _chkMatch |= 0b10;
+            } else if (this->_activeNode->vcc == pin->gnd) {
+                _downPin(pin->gnd);
+                _chkMatch |= 0b01;
+            } else {
+                if (this->_state)
+                    _sinkPin(this->_activeNode->vcc);
+            }
+//            if (this->_activeNode->vcc != pin->vcc && this->_activeNode->vcc != pin->gnd) {
+//                if (this->_state)
+//                    _sinkPin(this->_activeNode->vcc);
+//            }
+            //            switch (this->_activeNode->gnd) {
+            //                case pin->vcc:
+            //                    _upPin(pin->vcc);
+            //                    break;
+            //                case pin->gnd:
+            //                    break;
+            //                default:
+            //                    if (this->_state)
+            //                        _sinkPin(this->_activeNode->gnd);
+            //                    break;
+            //            };
+            if (this->_activeNode->gnd == pin->vcc) {
+                _upPin(pin->vcc);
+                _chkMatch |= 0b10;
+            } else if (this->_activeNode->gnd == pin->gnd) {
+                _chkMatch |= 0b01;
+            } else {
+                if (this->_state)
+                    _sinkPin(this->_activeNode->gnd);
+            }
+//            if (this->_activeNode->gnd != pin->vcc && this->_activeNode->gnd != pin->gnd) {
+//                if (this->_state)
+//                    _sinkPin(this->_activeNode->gnd);
+//            }
+            if((0b10 & _chkMatch) != 0b10){
+                _upPin(pin->vcc);
+            }
+            if((0b01 & _chkMatch) != 0b01){
+                _downPin(pin->gnd);
+            }
+//            _upPin(pin->vcc);
+//            _downPin(pin->gnd);
             this->_activeNode->vcc = pin->vcc;
             this->_activeNode->gnd = pin->gnd;
             this->_state = MXCHARLIE_ACTIVE;
+//            Serial.print("Supplied: ");
+//            Serial.print(pin->vcc);
+//            Serial.print("-");
+//            Serial.println(pin->gnd);
+//            Serial.print("Active: ");
+//            Serial.print(this->_activeNode->vcc);
+//            Serial.print("-");
+//            Serial.println(this->_activeNode->gnd);
             return true;
         } else { //The objective is to check the given Node doesn't get conflict
             uint8_t _chkMatch = 0; // Whether the given node is the ActiveNode
             uint8_t _chkConflict = 0; // Whether it conflicts with ActiveNode
             uint8_t _chkClear = 0; // Whether it doesn't conflict with ActiveNode
-            switch (this->_activeNode->vcc) {
-                case pin->vcc:
-                    _chkMatch = (_chkMatch << 1) | 1;
-                    break;
-                case pin->gnd:
-                    _chkConflict = (_chkConflict << 1) | 1;
-                    break;
-                default:
-                    _chkClear = (_chkClear << 1) | 1;
-                    break;
-            };
-            switch (this->_activeNode->gnd) {
-                case pin->vcc:
-                    _chkConflict = (_chkConflict << 1) | 1;
-                    break;
-                case pin->gnd:
-                    _chkMatch = (_chkMatch << 1) | 1;
-                    break;
-                default:
-                    _chkClear = (_chkClear << 1) | 1;
-                    break;
-            };
+//            switch (this->_activeNode->vcc) {
+//                case pin->vcc:
+//                    _chkMatch = (_chkMatch << 1) | 1;
+//                    break;
+//                case pin->gnd:
+//                    _chkConflict = (_chkConflict << 1) | 1;
+//                    break;
+//                default:
+//                    _chkClear = (_chkClear << 1) | 1;
+//                    break;
+//            };
+            if (this->_activeNode->vcc == pin->vcc) {
+                _chkMatch = (_chkMatch << 1) | 1;
+            } else if (this->_activeNode->vcc == pin->gnd) {
+                _chkConflict = (_chkConflict << 1) | 1;
+            } else {
+                _chkClear = (_chkClear << 1) | 1;
+            }
+
+//            switch (this->_activeNode->gnd) {
+//                case pin->vcc:
+//                    _chkConflict = (_chkConflict << 1) | 1;
+//                    break;
+//                case pin->gnd:
+//                    _chkMatch = (_chkMatch << 1) | 1;
+//                    break;
+//                default:
+//                    _chkClear = (_chkClear << 1) | 1;
+//                    break;
+//            };
+            if (this->_activeNode->vcc == pin->vcc) {
+                _chkConflict = (_chkConflict << 1) | 1;
+            } else if (this->_activeNode->vcc == pin->gnd) {
+                _chkMatch = (_chkMatch << 1) | 1;
+            } else {
+                _chkClear = (_chkClear << 1) | 1;
+            }
+
             if (0b11 == _chkClear) { // No harm in changing
                 _sinkPin(pin->vcc);
                 _sinkPin(pin->gnd);
@@ -144,30 +226,82 @@ namespace MatrixCharlieplex {
         return false;
     }
 
-    MatrixCharlieplex::_getNode(uint8_t row, uint8_t col) {
+    DiodeNode* MatrixCharlieplex::_getNode(uint8_t row, uint8_t col) {
         DiodeNode* node = new DiodeNode();
+        /* The objective is to map the exact pin as based on the Matrix. The 
+         * setup supports both Common-Cathode (CC) or Common-Anode (CA) grid.
+         * 
+         * Here Array are 0 based (i.e. starts the counts from 0 & then 1, 2, 3)
+         * whereas rows and columns are count from 1. Need a conversion here.
+         * 
+         * e.g.
+         *          Array: 0 | 1 | 2 | 3 | 4
+         *                 ^ | ^ | ^ | ^ | ^
+         *            Row: 1 | 2 | 3 | 4 | 5
+         *         Column: 1 | 2 | 3 | 4 | 5
+         */
+        node->vcc = *(this->_pins+(row - 1)); // same as row number
+        node->gnd = *(this->_pins+((col < row) ? (col - 1) : col)); // complicated
+//        Serial.print("Index: ");
+//        Serial.print(row - 1);
+//        Serial.print("-");
+//        Serial.println((col < row) ? (col - 1) : col);
+//        Serial.print("getNode: ");
+//        Serial.print(node->vcc);
+//        Serial.print("-");
+//        Serial.println(node->gnd);
+        return node;
+    }
+
+    DiodeNode* MatrixCharlieplex::_getNode(uint16_t index) {
+        DiodeNode* node = new DiodeNode();
+        /* This one is also calculated based on 0-based indexing like the above
+         * one.
+         */
+        uint8_t row = (index - 1) / (this->_noOfPins - 1);
+        uint8_t col = (index - 1) % (this->_noOfPins - 1);
+        node->vcc = *(this->_pins+row); // same as row number
+        node->gnd = *(this->_pins+((col < row) ? col : (col + 1))); // again complicated
         return node;
     }
 #pragma endregion Private Functions
 
 #pragma region Public Functions
 
-    MatrixCharlieplex::TurnOn(uint8_t row, uint8_t col) {
+    DiodeNode* MatrixCharlieplex::GetActiveNode() {
+        return this->_activeNode;
+    }
+
+    uint8_t* MatrixCharlieplex::GetPins() {
+        return this->_pins;
+    }
+
+    boolean MatrixCharlieplex::TurnOn(uint8_t row, uint8_t col) {
         return _setNode(_getNode(row, col), HIGH);
     }
 
-    MatrixCharlieplex::TurnOff(uint8_t row, uint8_t col) {
+    boolean MatrixCharlieplex::TurnOff(uint8_t row, uint8_t col) {
         return _setNode(_getNode(row, col), LOW);
     }
 
-    MatrixCharlieplex::Clear() {
-        return _setNode(this->_activeNode, LOW);
+    boolean MatrixCharlieplex::TurnOn(uint16_t index) {
+        return _setNode(_getNode(index), HIGH);
     }
 
-    MatrixCharlieplex::Reset() {
-        if (_setNode(this->_activeNode, LOW))
-            return _init(true);
-        return false;
+    boolean MatrixCharlieplex::TurnOff(uint16_t index) {
+        return _setNode(_getNode(index), LOW);
+    }
+
+    boolean MatrixCharlieplex::Clear() {
+        return ((this->_state == MXCHARLIE_INACTIVE) ? true : _setNode(this->_activeNode, LOW));
+    }
+
+    boolean MatrixCharlieplex::Reset() {
+        //if (_setNode(this->_activeNode, LOW))
+        //    return _init(true);
+        //return false;
+        _init(true);
+        return true;
     }
 #pragma endregion Public Functions
 }
